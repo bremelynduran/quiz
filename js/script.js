01f524493bff91193524ac06671a22081f81dba1
Bremelyn Duran <bremelynduran@ccpaedu.com>
2018-12-14 02:26:19 +0000

    The answers

--- js/script.js
+++ js/script.js
@@ -10,5 +10,73 @@ $(document).ready(function() {
         var q3Result = $("#question3").val();
         var totalScore;
     });
-
+ function sport(answer){
+     if (answer === "basketball" )
+     {
+         return 1;
+     }
+     if (answer === "Baseball")
+     {
+         return 2;
+     }
+     if (answer === "Soccer")
+     {
+         return 3;
+     }
+     if (answer === "Football")
+     {
+         return 4;
+     }
+ }
+ 
+    function snack(answer){
+        if (answer === "Candy")
+        {
+            return 1;
+        }
+        if (answer === "Cookies")
+        {
+            return 2;
+        }
+        if (answer === "Ice-Cream")
+        {
+            return 3;
+        }
+        if (answer === "Chips")
+        {
+            return 4;
+        }
+    }
+    
+    function sauce(answer){
+        if (answer === "No HotSauce")
+        {
+            return 1;
+        }
+        if (answer === "HotSauce")
+        {
+            return 4;
+        }
+    }
+    
+    function animal(answer)
+    {
+        if (answer === "Fish")
+        {
+            return 1;
+        }
+        if (answer === "Hamster")
+        {
+            return 2;
+        }
+        if (answer === "Cat")
+        {
+            return 3;
+        }
+        if (answer === "Dog")
+        {
+           return 4; 
+        }
+    }
 });
+
